8820aef50cd060a70c4ec04b29db2b24729b46f9
Burak Furkan Asilturk <furkan.asilturk@gmail.com>
2020-10-11 16:57:23 +0300

    ..

--- Case/Extensions/Date+Extensions.swift
+++ Case/Extensions/Date+Extensions.swift
@@ -9,27 +9,28 @@ import Foundation
 
 extension Date {
 
+    static func date(from dateString: String?) -> Date? {
+        guard let dateString = dateString else { return nil }
+
+        let formatter = DateFormatter()
+        formatter.timeZone = TimeZone.current
+        formatter.dateFormat = "yyyy-MM-dd"
+
+        return formatter.date(from: dateString)
+    }
+
     var onlyYear: String {
-        let formatter = DateFormatter(); formatter.dateFormat = "YYYY"; formatter.locale = Locale(identifier: "tr"); formatter.timeZone = TimeZone.current
+        let formatter = DateFormatter();
+        formatter.dateFormat = "YYYY";
+        formatter.timeZone = TimeZone.current
         return formatter.string(from: self as Date)
     }
 
     var shortDate: String {
-        let formatter = DateFormatter(); formatter.dateFormat = "dd.MM.yyyy"; formatter.locale = Locale(identifier: "tr"); formatter.timeZone = TimeZone.current
+        let formatter = DateFormatter();
+        formatter.dateFormat = "dd.MM.yyyy";
+        formatter.timeZone = TimeZone.current
         return formatter.string(from: self as Date)
-    }
-
-
-    /// Date degerinin utc eklenerek locale cevrilmis halidir.
-    /// - Returns: date ve dateString olarak deger doner.
-    static func date(from dateString: String?) -> Date? {
-        guard let dateString = dateString else { return nil }
-        
-        let dtf = DateFormatter()
-        dtf.timeZone = TimeZone.current
-        dtf.dateFormat = "yyyy-MM-dd"
-
-        return dtf.date(from: dateString)
     }
 
 }
